33d4d47714984c01629bcb0bd8b13f1e37cccc41
Yury <mr.bronium@gmail.com>
2025-12-04 05:18:34 +0300

    [2025] Add caching and improve memory allocation in day 3

--- 2025/3_2.js
+++ 2025/3_2.js
@@ -1,34 +1,48 @@
-BATTERY_LENGTH = 12
+const BATTERY_LENGTH = 12
+const DIGIT_MAP = Array.from({ length: 10 }, () => [])
 
-function findLargest(buffer, start, bank) {
-    if (buffer.toString().length === BATTERY_LENGTH) {
-        return buffer
-    }
+let cache
+let bank
 
-    const digitMap = Array.from({ length: 10 }, () => [])
+function clearDigitMap() {
+    DIGIT_MAP.forEach((arr) => arr.length = 0)
+}
+
+function findLargest(buffer, start) {
+    if (cache[buffer]) return cache[buffer]
+    if (buffer.toString().length === BATTERY_LENGTH) return buffer
+
+    clearDigitMap()
 
     for (let i = start; i < bank.length - (BATTERY_LENGTH - buffer.toString().length - 1); i++) {
-        digitMap[bank[i]].push(i)
+        DIGIT_MAP[bank[i]].push(i)
     }
 
     let ind = 0
-    const indices = digitMap.findLast((arr, i) => { ind = i; return arr.length > 0 })
+    const indices = DIGIT_MAP.findLast((arr, i) => { ind = i; return arr.length > 0 })
     if (!indices) return 0
 
     buffer = buffer * 10 + ind
-    results = indices.map((ind) => {
-        return findLargest(buffer, ind + 1, bank)
+    let max = 0
+    indices.forEach((ind) => {
+        const current = findLargest(buffer, ind + 1)
+        if (current > max) {
+            max = current
+        }
     })
 
-    return Math.max(...results)
+    cache[buffer] = max
+    return max
 }
 
 const fs = require('node:fs');
 const input = fs.readFileSync('./input.txt', 'utf8');
 
 let sum = 0
-input.split('\n').forEach((bank) => {
-    sum += findLargest('', 0, bank.split('').map(num => parseInt(num)))
+input.split('\n').forEach((line) => {
+    cache = []
+    bank = line.split('').map(num => parseInt(num))
+    sum += findLargest('', 0)
 })
 
 console.log("Sum: ", sum)
